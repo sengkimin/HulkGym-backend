e19349015e69c5c6fb0cb49e1ede56c7cc13c69d
SopheaII <45730417+SopheaII@users.noreply.github.com>
2025-02-10 16:15:36 +0700

    update

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -31,12 +31,6 @@ export const register = async (req: Request, res: Response) => {
 
   const token = generateToken({ id: user.id, role: RoleEnum[2] });
 
-  res.cookie('token', token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: 'none',
-  });
-
   return res
     .status(200)
     .json({ message: "User created successfully" });
@@ -70,12 +64,7 @@ export const login = async (req: Request, res: Response) => {
       return res.status(404).json({ message: "User not found" });
     }
     const token = generateToken({ id: user.id, role: user.role as RoleType });
-    res.cookie('token', token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'none',
-    });
-    return res.status(200).json({ message: "Login Successfully" });
+    return res.status(200).json({ message: "Login Successfully", token });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal server error" });
