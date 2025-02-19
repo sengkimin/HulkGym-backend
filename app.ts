acb0ff53f5d69cef2560940a189f8cfc3f5eb0a1
Soychanleakhena <chanleakhena.soy@institute.pse.ngo>
2025-02-19 04:59:01 -0500

    contact

--- app.ts
+++ app.ts
@@ -12,6 +12,8 @@ import activity from "./src/routes/activity";
 import telegramBot from "node-telegram-bot-api";
 import { handleMessage } from "./src/service/telegram.service";
 import axios from "axios";
+import  contact from "./src/routes/contact";
+import { configDotenv } from "dotenv";
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TELEGRAM_TOKEN || "";
@@ -34,6 +36,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 // Routes setuphttps://fboxmschac.sharedwithexpose.com
 app.use("/api/auth", auth);
 app.use("/api/activity", activity);
+app.use("/api/contact", contact);
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new telegramBot(token, { polling: true });
